810fd77a761a39981406463600783a945c5c1647
0tkl <118708188+0tkl@users.noreply.github.com>
2025-07-08 18:45:29 +0800

    1

--- src/dialog_search_replace.cpp
+++ src/dialog_search_replace.cpp
@@ -63,12 +63,14 @@ DialogSearchReplace<has_replace>::DialogSearchReplace(agi::Context* c)
 
 	auto find_sizer = new wxFlexGridSizer(2, 2, 5, 15);
 	find_edit = new wxComboBox(this, -1, "", wxDefaultPosition, wxSize(300, -1), recent_find, wxCB_DROPDOWN | wxTE_PROCESS_ENTER, StringBinder(&settings->find));
+	find_edit->SetHint("Find what...");
 	find_edit->SetMaxLength(0);
 	find_sizer->Add(new wxStaticText(this, -1, _("Find what:")), wxSizerFlags().Center().Left());
 	find_sizer->Add(find_edit);
 
 	if (has_replace) {
 		replace_edit = new wxComboBox(this, -1, "", wxDefaultPosition, wxSize(300, -1), lagi_MRU_wxAS("Replace"), wxCB_DROPDOWN | wxTE_PROCESS_ENTER, StringBinder(&settings->replace_with));
+		replace_edit->SetHint("Replace with...");
 		replace_edit->SetMaxLength(0);
 		find_sizer->Add(new wxStaticText(this, -1, _("Replace with:")), wxSizerFlags().Center().Left());
 		find_sizer->Add(replace_edit);
